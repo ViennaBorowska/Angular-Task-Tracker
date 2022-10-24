c1daba71fc74cabe7fca7d64ebb2060c8fdff4b9
Vienna B <viennabdev@gmail.com>
2022-10-24 11:40:59 +0100

    add-task function is fully working - added to taskService

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -29,6 +29,6 @@ export class TasksComponent implements OnInit {
   }
 
   addTask(task: Task) {
-    console.log(task);
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
   }
 }
